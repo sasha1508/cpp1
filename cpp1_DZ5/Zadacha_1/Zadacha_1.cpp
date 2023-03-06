fa325ac570eebc0b53b0b1ec2bd095f9a4dccd83
aleksandr.ilyin <sasha8@list.ru>
2023-03-06 21:07:05 +0300

    Задача 1 выполнена

---
+++ cpp1_DZ5/Zadacha_1/Zadacha_1.cpp
@@ -0,0 +1,23 @@
+﻿// Написать функцию которая выводит массив double чисел на экран.
+// Параметры функции это сам массив и его размер.Вызвать эту функцию из main.
+
+#include <iostream>
+
+using namespace std;
+
+
+void printArray(double array[], int size)
+{
+    for (size_t i = 0; i < size; i++)
+    {
+        cout << array[i] << " | ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    const int size = 5;
+    double array[size] = { 1.7,4,5.85,7,2 };
+    printArray(array, size);
+}
