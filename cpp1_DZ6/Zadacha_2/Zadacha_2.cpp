723be3430eea98d1d9c06d9944ecdf911194974f
aleksandr.ilyin <sasha8@list.ru>
2023-03-12 18:36:45 +0300

    Задача 6_2 выполнена

---
+++ cpp1_DZ6/Zadacha_2/Zadacha_2.cpp
@@ -0,0 +1,49 @@
+﻿// Динамически выделить матрицу 4х4 типа int.
+// Инициализировать ее псевдослучайными числами через функцию rand.Вывести на экран.
+// Разбейте вашу программу на функции которые вызываются из main.
+//
+#include <iostream>
+using namespace std;
+
+void AddRandom(int** pMatrix, size_t SIZE)
+{
+	for (size_t i = 0; i < SIZE; i++)
+	{
+		for (size_t j = 0; j < SIZE; j++)
+		{
+			pMatrix[i][j] = rand();
+		}
+	}
+}
+
+void PrintMatrix(int** pMatrix, size_t SIZE)
+{
+	for (size_t i = 0; i < SIZE; i++)
+	{
+		for (size_t j = 0; j < SIZE; j++)
+		{
+			cout << pMatrix[i][j] << "\t";
+		}
+		cout << endl;
+	}
+}
+
+int main()
+{
+    size_t SIZE = 4;
+    int** pMatrix = new int* [SIZE]; // Выделение массива указателей на строки
+	for (size_t i = 0; i < SIZE; i++) // Выделение строк матрицы
+	{
+		pMatrix[i] = new int[SIZE];
+	}
+
+	AddRandom(pMatrix, SIZE);
+	PrintMatrix(pMatrix, SIZE);
+
+	for (size_t i = 0; i < SIZE; i++) // Освобождение строк матрицы из памяти
+	{
+		delete[] pMatrix[i];
+	}
+	delete[] pMatrix; // Удаление из памяти массива указателей на строки 
+}
+
