361a3c864c3d8c4e1e4879ceecce2bf5f1cb3761
aleksandr.ilyin <sasha8@list.ru>
2023-03-12 20:42:51 +0300

    Задача 6_5 выполнена

---
+++ cpp1_DZ6/Zadacha_5/Zadacha_5.cpp
@@ -0,0 +1,34 @@
+﻿// * Написать программу, которая проверяет присутствует ли указанное пользователем при запуске программы слово 
+// в указанном пользователем файле (для простоты работаем только с латиницей). 
+// Используем функцию find которая есть в строках std::string.
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+string ReadFile(string Name)
+{
+    ifstream fout11(Name);
+    string buf;
+    string buf1;
+
+    while (!fout11.eof())
+    {
+        getline(fout11, buf);
+        buf1 += buf;
+    }
+    fout11.close();
+
+    return buf1;
+}
+
+int main()
+{
+    string test = ReadFile("Test.txt");
+    string slovo = "file";
+    if (test.find(slovo) != string::npos) cout << "Yes";
+    else cout << "No";
+}
