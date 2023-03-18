a874c7c6f968b1a5af8a96b0911ff218fc01a6ec
Александр Ильин <sasha8@list.ru>
2023-03-18 16:38:05 +0300

    Первая задача сделана

---
+++ cpp1_DZ7/Zadacha_1/Zadacha_1/main.cpp
@@ -0,0 +1,24 @@
+﻿// Создайте проект из 2х cpp файлов и заголовочного (main.cpp, mylib.cpp, mylib.h) 
+// во втором модуле mylib объявить 3 функции: 
+// - для инициализации массива (типа float), 
+// - печати его на экран 
+// - и подсчета количества отрицательных и положительных элементов. 
+// Вызывайте эти 3-и функции из main для работы с массивом.
+//
+
+#include <iostream>
+#include "mylib.h"
+#include <locale.h>  // Подключение русского языка
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian"); // Подключение русского языка
+    const size_t size = 10;
+    float Array[size];
+    insertToArray(Array, size);
+    printArray(Array, size);
+    pozitiveNegativeCount(Array, size);
+ 
+}
---
+++ cpp1_DZ7/Zadacha_1/Zadacha_1/mylib.h
@@ -0,0 +1,6 @@
+#pragma once
+
+float* insertToArray(float array[], size_t size);
+void printArray(float array[], int size);
+float random(int a, int b);
+void pozitiveNegativeCount(float array[], int size);
