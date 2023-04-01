cc4622cd7c54840dc0491633dc23e0e7d629fbd0
Александр Ильин <sasha8@list.ru>
2023-04-01 12:26:02 +0300

    Первая гоовность игры

--- cpp1_DZ8/cpp1_DZ8/cpp1_DZ8.cpp
+++ cpp1_DZ8/cpp1_DZ8/cpp1_DZ8.cpp
@@ -38,8 +38,8 @@ struct TCoord
 struct TGame {
     TCell** ppField{nullptr};
     const size_t size{ 3 }; // 3x3
-    TCell human;
-    TCell ai;
+    TCell human{CROSS};
+    TCell ai{ZERO};
     size_t turn{ 0 }; // Чётные - человек, нечётные - компьютер
     TProgress progress{ IN_PROGRESS };
 };
@@ -121,8 +121,166 @@ void printGame(const TGame& g)
         }
         cout << endl;
     }
-    cout << endl << "Human: " << g.human << endl << "Computer: " << g.ai << endl;
+    cout << endl << "Human: " << g.human << endl << "Computer: " << g.ai << endl << endl;
 }
+
+TProgress getWon(const TGame& g)
+{
+    //Выигрыш в строках
+    for (size_t y = 0; y < g.size; y++)
+    {
+        if (g.ppField[y][0] == g.ppField[y][1] && g.ppField[y][0] == g.ppField[y][2])
+        {
+            if (g.ppField[y][0] == g.human)
+            {
+                return WON_HUMAN;
+            }
+            if (g.ppField[y][0] == g.ai)
+            {
+                return WON_AI;
+            }
+        }
+    }
+    //Выигрыш в столбцах:
+    for (size_t x = 0; x < g.size; x++)
+    {
+        if (g.ppField[0][x] == g.ppField[1][x] && g.ppField[0][x] == g.ppField[2][x])
+        {
+            if (g.ppField[0][x] == g.human)
+            {
+                return WON_HUMAN;
+            }
+            if (g.ppField[0][x] == g.ai)
+            {
+                return WON_AI;
+            }
+        }
+    }
+    //Выигрыш в диагоналях:
+    if (g.ppField[0][0] == g.ppField[1][1] && g.ppField[0][0] == g.ppField[2][2])
+    {
+        if (g.ppField[0][0] == g.human)
+        {
+            return WON_HUMAN;
+        }
+        if (g.ppField[0][0] == g.ai)
+        {
+             return WON_AI;
+        }
+    }
+    if (g.ppField[2][0] == g.ppField[1][1] && g.ppField[2][0] == g.ppField[0][2])
+    {
+        if (g.ppField[1][1] == g.human)
+        {
+            return WON_HUMAN;
+        }
+        if (g.ppField[1][1] == g.ai)
+        {
+            return WON_AI;
+        }
+    }
+    //Ничья:
+    bool draw{ true };
+    for (size_t y = 0; y < g.size; y++)
+    {
+        for (size_t x = 0; x < g.size; x++)
+        {
+            if (g.ppField[y][x] = EMPTY)
+            {
+                draw = false;
+                break;
+            }
+        }
+        if (!draw)
+            break;
+    }
+    if (draw)
+        return DRAW;
+   
+    return IN_PROGRESS;
+}
+
+TCoord getHumanCoord(const TGame& g)
+{
+    TCoord c;
+    do {
+        cout << "Enter X (1..3): ";
+        cin >> c.x;
+        cout << "Enter Y (1..3): ";
+        cin >> c.y;
+        c.x--;
+        c.y--;
+    } while (c.x > 2 || c.y > 2 || g.ppField[c.x][c.y] != EMPTY);
+
+    return c;
+}
+TCoord getAICoord(TGame& g)
+{
+    //Ходы по приоритетам  и рандомайз по ним
+    //Центр:
+    if (g.ppField[1][1] == EMPTY)
+    {
+        return { 1, 1 };
+    }
+
+    //Углы:
+    TCoord buf[4];
+    size_t num{ 0 };
+    if (g.ppField[0][0] == EMPTY)
+    {
+        buf[num] = { 0,0 };
+        num++;
+    }
+    if (g.ppField[2][2] == EMPTY)
+    {
+        buf[num] = { 2,2 };
+        num++;
+    }
+    if (g.ppField[0][2] == EMPTY)
+    {
+        buf[num] = { 0,2 };
+        num++;
+    }
+    if (g.ppField[2][0] == EMPTY)
+    {
+        buf[num] = { 2,0 };
+        num++;
+    }
+    if (num > 0)
+    {
+        const size_t index = rand() % num;
+        return buf[index];
+    }
+
+    //Не углы:
+    num = 0;
+    if (g.ppField[0][1] == EMPTY)
+    {
+        buf[num] = { 0,1 };
+        num++;
+    }
+    if (g.ppField[1][0] == EMPTY)
+    {
+        buf[num] = { 1,0 };
+        num++;
+    }
+    if (g.ppField[1][2] == EMPTY)
+    {
+        buf[num] = { 1,2 };
+        num++;
+    }
+    if (g.ppField[2][1] == EMPTY)
+    {
+        buf[num] = { 2,1 };
+        num++;
+    }
+    if (num > 0)
+    {
+        const size_t index = rand() % num;
+        return buf[index];
+    }
+}
+
 
 //====================================================================================
 
@@ -131,7 +289,29 @@ int main()
     TGame g;
     initGame(g);
 
+    clearScr();
     printGame(g);
+
+    do {
+        if (g.turn % 2 == 0)
+        {
+            //Ходит человек
+            TCoord c = getHumanCoord(g);
+            g.ppField[c.y][c.x] = g.human;
+        }
+        else
+        {
+            //Ходит компьютер
+            TCoord c = getAICoord(g);
+            g.ppField[c.y][c.x] = g.ai;
+        }
+
+        clearScr();
+        printGame(g);
+        g.turn++;
+        g.progress = getWon(g);
+
+    } while (g.progress == IN_PROGRESS);
 
     deinitGame(g);
 
