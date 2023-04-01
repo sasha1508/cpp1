7ac4dff912ea3909408ad1feedba7397c5e3937a
Александр Ильин <sasha8@list.ru>
2023-04-01 10:02:52 +0300

    Добавлен генератор случайных чисел

--- cpp1_DZ8/cpp1_DZ8/cpp1_DZ8.cpp
+++ cpp1_DZ8/cpp1_DZ8/cpp1_DZ8.cpp
@@ -7,17 +7,84 @@
 //
 
 #include <iostream>
+#include <random>
+#include <stdlib.h>
+#include <chrono>
 
 using namespace std;
 
-enum Cell : char {
+enum TCell : char {
     CROSS = 'X',
-    ZERO = '0',
+    ZERO = 'O',
     EMPTY = '_'
 
 };
 
+enum TProgress {
+    IN_PROGRESS,
+    WON_HUMAN,
+    WON_AI,
+    DRAW
+};
+
+struct TCoord
+{
+    size_t y;
+    size_t x;
+};
+
+struct TGame {
+    TCell** ppField{nullptr};
+    const size_t size{ 3 }; // 3x3
+    TCell humsn;
+    TCell ai;
+    size_t turn{ 0 }; // Чётные - человек, нечётные - компьютер
+    TProgress progress{ IN_PROGRESS };
+};
+
+void clearScr()
+{
+    // system("cls");
+    cout << "\x1B[2j\x1B[H";
+}
+
+int32_t getRendomNum(int32_t min, int32_t max)
+{
+    const static auto seed = chrono::system_clock::now().time_since_epoch().count();
+    static mt19937_64 generator(seed);
+    uniform_int_distribution<int32_t> dis(min, max);
+    return dis(generator);
+}
+
+void initGame(TGame & g)
+{
+    g.ppField = new TCell * [g.size];
+    for (size_t i = 0; i < g.size; i++)
+    {
+        g.ppField[i] = new TCell[g.size];
+    }
+
+    for (size_t y = 0; y  < g.size; y ++)
+    {
+        for (size_t x = 0; x < g.size; x++)
+        {
+            g.ppField[y][x] = EMPTY;
+        }
+    }
+}
+
+
+void deinitGame(TGame & g)
+{
+    
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    TGame g;
+    initGame(g);
+
+
+    deinitGame(g);
+
 }
